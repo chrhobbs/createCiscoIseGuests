058d836a0d0b65136ad7a7fb76404a5c329e0b42
chrhobbs <chrhobbs@users.noreply.github.com>
2017-11-21 10:39:07 -0500

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-# Enter Cisco ISE Information
-sponsorUsername = 'api-sponsor'
-sponsorPassword = 'C1sco12345'
-ersUsername = 'ers-admin'
-ersPassword = 'C1sco12345'
-host = '198.18.133.27'      # Use ISE PAN IP Address
